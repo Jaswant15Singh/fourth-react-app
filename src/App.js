53d7e43dcd69fc8577df4cf29685683fe8dcc1e9
Jaswant Singh <singhjaswant0932@gmail.com>
2023-06-15 11:34:03 +0530

    b2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
+import { useState } from "react";
+
 function App(){
   return(
     <>
-    <h1>hello</h1>
-    <MessageDemo message="jas"/>
-    <MessageDemo message="yash"/>
-    <MessageDemo message="amar"/>
+    <h1>Stateful list</h1>
+    <ListDemo/>
     </>
   );
 }
 
-function MessageDemo({message,name}){
-  console.log("hii",message);
-  return(
-    <>
-    <h1>Hello {message}</h1>
-    </>
+function ListDemo(){
+  
+// let data=["Good Morning"];
+let [list,setList]=useState(["Jaswant"]);
+
+let addItemAcction = () => {
+  let inputElement=document.querySelector("#id1");
+
+  let inputValue=inputElement.value;
+  let newList=[...list,inputValue];
+  console.log(newList);
+  setList(newList);
+  inputElement.value="";
+};
+
+return(
+  <>
+  <input type="text" id="id1" placeholder="WhatsApp"/>
+  <input type="button" value="Add New Item" onClick={addItemAcction} />
+  {list.map((item)=>
+  (<h1>{item}</h1>))}
+      </>
   );
 }
 export default App;
